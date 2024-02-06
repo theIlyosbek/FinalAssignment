ba0853cbad872915b6f6b528607db12a63efbf74
theIlyosbek <ilyosbeknajmiddinov055@gamil.com>
2024-02-06 18:56:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def roman_to_int(s):
+    rnums = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    result = 0
+    i = 0
+
+    while i < len(s):
+        if i + 1 < len(s) and rnums[s[i]] < rnums[s[i + 1]]:
+            result += rnums[s[i + 1]] - rnums[s[i]]
+            i += 2
+        else:
+            result += rnums[s[i]]
+            i += 1
+
+    return result
+
+
+# Example usage:
+roman_numeral = "DCXXI"
+result_integer = roman_to_int(roman_numeral)
+print(f"The integer equivalent of {roman_numeral} is {result_integer}")
